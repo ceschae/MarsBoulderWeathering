87b1b72ad2d9b75f50ebf1118fe0d556bd1e1137
Caitlin Schaefer <Caitlin Schaefer>
2017-05-09 23:20:03 -0700

    v 0.5: added resistance to overlapping boulders

--- code.py
+++ code.py
@@ -1,25 +1,24 @@
 # Mars boulder weathering simulation
-# version: 0.4
+# version: 0.5
 # author: Caitlin Schaefer (ceschae@gmail.com)
-# last updated: 3 May 2017
+# last updated: 9 May 2017
 #
-# weathers a NUMBER of basalt rocks for a given amount of TIME in days
+# weathers a NUMBER of basalt rocks in random locations from -WINDOW_SIZE
+# to +WINDOW_SIZE in both x and y with random radii from MIN_RADIUS to 
+# MAX_RADIUS (excluding overlapping boulders) for a given amount of TIME in days
 # using aeolian weathering model, outputs before/after rock graph as a 
-# result of weathering. random input x, y, radius data set 
+# result of weathering.
 #
 # to run on my machine:
-# C:\Users\cesch\AppData\Local\Programs\Python\Python36-32\python.exe C:\Users\cesch\Documents\2016-2017\MarsBoulderWeathering\code.py
+# C:\Users\cesch\AppData\Local\Programs\Python\Python36-32\python.exe C:\Users\cesch\Documents\2016-2017\Research\MarsBoulderWeathering\code.py
 # after running, can find image produced at in file as temp-plot.html
 
 # TODO List:
-#	- get rid of overlapping rocks
 #	- movie of plot with changes?
 #	- CFA plot
 #	- reformat title
 #	- size graphs
-#	- implement overlap search
 #	- consider implementing own random distributions
-
 
 import numpy as np
 # numpy may become important later, as may scipy
@@ -30,7 +29,7 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 NUMBER = 1000 # a thousand rocks
-TIME = 2000000000 # 2 billion years
+TIME = 15000000000 # 15 billion years
 WINDOW_SIZE = 100 # 100 m x 100 m
 MAX_RADIUS = 10 # 10 m
 MIN_RADIUS = 0.25 # 0.25 m
@@ -69,7 +68,13 @@ class BasaltRock:
 		self.new_radius = self.radius - years * 0.04 * 10**(-9)
 
 	def is_overlapping(new_rock):
-		return True
+		for rock in BasaltRock.rocks:
+			if ((rock.x < new_rock.x) and (rock.x + rock.radius > new_rock.x - new_rock.radius)) \
+					or ((rock.x > new_rock.x) and (new_rock.x + new_rock.radius > rock.x - rock.radius)) \
+					or ((rock.y < new_rock.y) and (rock.y + rock.radius > new_rock.y - new_rock.radius)) \
+					or ((rock.y > new_rock.y) and (new_rock.y + new_rock.radius > rock.y - rock.radius)):
+				return True
+		return False
 
 x_small = np.random.random_sample(NUMBER) 
 x = [n * WINDOW_SIZE * 2 - WINDOW_SIZE for n in x_small] # x ranges from -WINDOW_SIZE to +WINDOW_SIZE
@@ -84,19 +89,19 @@ for i in range(NUMBER):
 		radii_small[i] = np.random.random_sample(1)[0] * MAX_RADIUS
 	radii.append(radii_small[i])
 	rock = BasaltRock(x[i],y[i],radii[i],0)
-	if BasalTRock.is_overlapping(rock) is False:
+	if BasaltRock.is_overlapping(rock) is False:
 		BasaltRock.rocks.append(rock)
 
 # for testing weathering products
-for rock in rocks:
+for rock in BasaltRock.rocks:
 	rock.aeolian_weather(TIME)
 	BasaltRock.original_radii.append(rock.radius)
 	BasaltRock.new_radii.append(rock.new_radius)
 
 orig_r = [r * 20 for r in BasaltRock.original_radii]
 new_r = [r * 20 for r in BasaltRock.new_radii]
-print(orig_r[0])
-print(new_r[0])
+print(len(orig_r))
+print(len(new_r))
 data = [
     go.Scatter(
 		x=x,
